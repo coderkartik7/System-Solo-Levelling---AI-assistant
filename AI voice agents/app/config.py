2659603e36ef0b0c55a5ffa7c4744137953bcce7
Coder Kartik <kartikgarg271005@gmail.com>
2025-08-23 01:41:54 +0530

    Added Murf(TTS) API key

--- AI voice agents/app/config.py
+++ AI voice agents/app/config.py
@@ -2,7 +2,7 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-
+MURF_API_KEY = os.getenv("MURF_API_KEY")
 ASSEMBLY_API_KEY = os.getenv("ASSEMBLY_API_KEY")
 GEMINI_API_KEY = os.getenv("G_API_KEY")
 HOST = "127.0.0.1"
