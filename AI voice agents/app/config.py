38a582b988f20e47411ee76913f61617f6fa20a9
Coder Kartik <kartikgarg271005@gmail.com>
2025-08-29 23:17:00 +0530

    Search Api Key

--- AI voice agents/app/config.py
+++ AI voice agents/app/config.py
@@ -5,8 +5,9 @@ load_dotenv()
 MURF_API_KEY = os.getenv("MURF_API_KEY")
 ASSEMBLY_API_KEY = os.getenv("ASSEMBLY_API_KEY")
 GEMINI_API_KEY = os.getenv("G_API_KEY")
-HOST = "127.0.0.1"
-PORT = 8000
+SEARCH_API_KEY = os.getenv("SEARCH_API_KEY")
+HOST = "0.0.0.0"
+PORT = int(os.getenv("PORT",8000))
 
 if not ASSEMBLY_API_KEY or not GEMINI_API_KEY:
     raise ValueError("Missing required API keys: ASSEMBLY_API_KEY, G_API_KEY")
