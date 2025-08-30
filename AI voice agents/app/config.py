80af107cc85ed6a5b33eacc03014a8ae0f680a4c
Coder Kartik <kartikgarg271005@gmail.com>
2025-08-30 07:47:15 +0530

    T

--- AI voice agents/app/config.py
+++ AI voice agents/app/config.py
@@ -9,11 +9,11 @@ SEARCH_API_KEY = os.getenv("SEARCH_API_KEY")
 HOST = "0.0.0.0"
 PORT = int(os.getenv("PORT",8000))
 
-if not ASSEMBLY_API_KEY:
-    raise ValueError("Missing ASSEMBLY_API_KEY")
-if not GEMINI_API_KEY:
-    raise ValueError("Missing GEMINI_API_KEY")
-if not MURF_API_KEY:
-    raise ValueError("Missing MURF_API_KEY")
-if not SEARCH_API_KEY:
-    raise ValueError("Missing SEARCH_API_KEY")
+# if not ASSEMBLY_API_KEY:
+#     raise ValueError("Missing ASSEMBLY_API_KEY")
+# if not GEMINI_API_KEY:
+#     raise ValueError("Missing GEMINI_API_KEY")
+# if not MURF_API_KEY:
+#     raise ValueError("Missing MURF_API_KEY")
+# if not SEARCH_API_KEY:
+#     raise ValueError("Missing SEARCH_API_KEY")
